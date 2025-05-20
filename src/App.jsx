e55fec0f517027c935c05783c49e252846d851ad
NICOLAS CORRÊA GUBERT <nicolas_gubert@estudante.sesisenai.org.br>
2025-05-20 20:36:33 -0300

    Adicionado inícil da secção de perfil.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Cadastro from './pages/Cadastro'
 import Login from './pages/Login'
+import Perfil from './pages/Perfil'
 
 function App() {
   const [pagina, setPagina] = useState(<Login />)
@@ -14,6 +15,7 @@ function App() {
         </div>
 
         <div className='container-botoes-app'>
+          <button className='botao-ir-perfil' onClick={() => setPagina(<Perfil />)}>Perfil</button>
           <button className='botao-ir-cad' onClick={() => setPagina(<Cadastro />)}>Cadastro</button>
           <button className='botao-ir-log' onClick={() => setPagina(<Login />)}>Login</button>
         </div>
